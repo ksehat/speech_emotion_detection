41472f448f69f7a1de4e6bd571fd3fdfc4b03673
k.sehat <k.sehat145@gmail.com>
2023-12-12 09:47:39 +0330

    data preparation for initial datasets.

--- kaggle_dataset_path_emotion_dataframe_creator.py
+++ kaggle_dataset_path_emotion_dataframe_creator.py
@@ -8,11 +8,10 @@ Tess = "D:\speech_emotion_detection\data\kaggle dataset/tess\TESS Toronto emotio
 Savee = "D:\speech_emotion_detection\data\kaggle dataset\savee\ALL/"
 
 ravdess_directory_list = os.listdir(Ravdess)
-
 file_emotion = []
 file_path = []
 for dir in ravdess_directory_list:
-    # as their are 20 different actors in our previous directory we need to extract files for each actor.
+    # as there are 20 different actors in our previous directory we need to extract files for each actor.
     actor = os.listdir(Ravdess + dir)
     for file in actor:
         part = file.split('.')[0]
@@ -21,15 +20,13 @@ for dir in ravdess_directory_list:
         file_emotion.append(int(part[2]))
         file_path.append(Ravdess + dir + '/' + file)
 
-# dataframe for emotion of files
 emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
-
-# dataframe for path of files.
 path_df = pd.DataFrame(file_path, columns=['path'])
 Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
 Ravdess_df.emotion.replace(
     {1: 'neutral', 2: 'calm', 3: 'happy', 4: 'sad', 5: 'angry', 6: 'fear', 7: 'disgust', 8: 'surprise'}, inplace=True)
-# Ravdess_df.to_csv('ravdess_path_emotion.csv')
+
+
 
 crema_directory_list = os.listdir(Crema)
 file_emotion = []
@@ -57,7 +54,8 @@ for file in crema_directory_list:
 emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
 path_df = pd.DataFrame(file_path, columns=['path'])
 Crema_df = pd.concat([emotion_df, path_df], axis=1)
-# Crema_df.to_csv('cremad_path_emotion.csv')
+
+
 
 tess_directory_list = os.listdir(Tess)
 file_emotion = []
@@ -76,7 +74,8 @@ for dir in tess_directory_list:
 emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
 path_df = pd.DataFrame(file_path, columns=['path'])
 Tess_df = pd.concat([emotion_df, path_df], axis=1)
-# Tess_df.to_csv('tess_path_emotion.csv')
+
+
 
 savee_directory_list = os.listdir(Savee)
 file_emotion = []
@@ -103,7 +102,6 @@ for file in savee_directory_list:
 emotion_df = pd.DataFrame(file_emotion, columns=['emotion'])
 path_df = pd.DataFrame(file_path, columns=['path'])
 Savee_df = pd.concat([emotion_df, path_df], axis=1)
-# Savee_df.to_csv('savee_path_emotion.csv')
 
 # Define the path to your folder containing the audio files
 folder_path = 'D:\speech_emotion_detection\data/org audio files'
@@ -124,7 +122,6 @@ Shemo_df = pd.DataFrame({
 })
 Shemo_df.emotion.replace(
     {'N': 'neutral', 'H': 'happy', 'S': 'sad', 'A': 'angry', 'F': 'fear', 'W': 'surprise'}, inplace=True)
-# Shemo_df.to_csv('shemo_path_emotion.csv')
 
 final_df = pd.concat([Ravdess_df,
                       Crema_df,
